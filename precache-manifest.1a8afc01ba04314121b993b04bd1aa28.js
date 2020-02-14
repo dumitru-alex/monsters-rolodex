f3a307ef2d6ec52caf60864383b7208463d1780c
Alexandru-Paul Dumitru <alexandru.dumitru@broadcom.com>
2020-02-14 11:31:53 +0100

    Updates

---
+++ precache-manifest.1a8afc01ba04314121b993b04bd1aa28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3210ddcf6447975bec03a699dc62adac",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1471452412f70e44a549",
+    "url": "/monsters-rolodex/static/css/main.e9c76097.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1471452412f70e44a549",
+    "url": "/monsters-rolodex/static/js/main.20f37720.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
